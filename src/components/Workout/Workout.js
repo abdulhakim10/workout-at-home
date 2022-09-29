7e143efede048cf21e31be8f3728fbe0ae32cfe5
syed abdul hakim <s.abdulhakim10@gmail.com>
2022-09-29 14:40:09 +0600

    data loaded into workout component

--- src/components/Workout/Workout.js
+++ src/components/Workout/Workout.js
@@ -6,10 +6,12 @@ const Workout = (props) => {
     return (
         <div className='workout-info'>
             <img src={image} alt="" />
+           <div className="details">
            <h3>{name}</h3>
            <p>For Age: 18 - 35 </p>
-           <p>Duration: {duration} min </p>
+           <p>Time Required: {duration} min </p>
            <button>Add To Chart</button>
+           </div>
         </div>
     );
 };
